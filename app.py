7504d63d5428cfeb0a3bbd950a5577115b609af1
Drew <dandadrew02@gmail.com>
2023-03-25 13:01:27 -0500

    can input stocks'

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return "Hello World"
+
+@app.route('/stockAnalysis', methods=["GET", "POST"])
+def stockAnalysis():
+    if request.method == "POST":
+        print(request.form["stock"])
+        print(request.form["startDate"])
+        print(request.form["endDate"])
+        print(type(request.form['startDate']))
+        return "printed";
+    else:
+        return render_template("InputStocks.html");
 
 
 if __name__ == '__main__':
